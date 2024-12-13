04cbb843b2416acf32b6d262211e1908f9b07682
Tobias Rønningen <tobias.ron@live.no>
2024-12-13 10:51:04 +0100

    Based on feedback, I changed some things around, so now it is possible to run the simulation by using one function and just changing the state parameter.

--- main.cpp
+++ main.cpp
@@ -2,27 +2,34 @@
 #include <vector>
 #include <chrono>
 
-void withoutFlush(int length, int xLines){
+void runSimulation(int length, int xLines, int state){
 
     using namespace std::chrono;
 
     std::pmr::vector<long long> timeTaken = {};
 
     for (int j = 0; j < length; j++) {
-
+        // Starts the timer.
         auto start = high_resolution_clock::now();
-
         for (int i = 0; i < xLines; i++) {
-            std::cout << "This is some text without flushing the buffer.\n";
-        }
+                if (state == 1) {
+                    std::cout << "This is some text without flushing the buffer.\n";
+                } else if (state == 2){
+                    std::cout << "This is some text with flushing the buffer." << std::endl;
+                } else {
+                    std::cout << "Invalid state.";
+                    break;
+                }
+            }
 
+        // Stops the timer and gets the duration by getting the difference between the start and stop time.
         auto stop = high_resolution_clock::now();
-
         auto duration = duration_cast<microseconds>(stop - start);
-
+        // Adds the duration in the vector.
         timeTaken.push_back(duration.count());
 
-    }
+        }
+
 
     long long totalTime = 0;
 
@@ -40,43 +47,6 @@ void withoutFlush(int length, int xLines){
 
     std::cout << "Average time is: " << averageTime << "ms" << std::endl;
 
-}
-
-void withFlush(int length, int xLines){
-    using namespace std::chrono;
-
-    std::pmr::vector<long long> timeTaken = {};
-
-    for (int j = 0; j < length; j++) {
-
-        auto start = high_resolution_clock::now();
-
-        for (int i = 0; i < xLines; i++) {
-            std::cout << "This is some text with flushing the buffer." << std::endl;
-        }
-
-        auto stop = high_resolution_clock::now();
-
-        auto duration = duration_cast<microseconds>(stop - start);
-
-        timeTaken.push_back(duration.count());
-
-    }
-
-    long long totalTime = 0;
-    for (const auto& time : timeTaken) {
-        totalTime += time;
-    }
-
-    /* long long guarantees same amounts of bits on both 32- and 64-bit systems.
-     * const unsigned long long, because it is never supposed to have a negative number.
-     * Calculates the time based on the total amount of time all the simulaitons took and divides
-     * it by the number of simulations ran. Then you get how much time one simulation took, and then
-     * you take the average of all those times and store it in the averageTime var.
-    */
-    const unsigned long long averageTime = totalTime / timeTaken.size();
-
-    std::cout << "Average time is: " << averageTime << "ms" << std::endl;
 }
 
 int main(){
@@ -84,8 +54,8 @@ int main(){
     const int timesToRun = 10000;     // Decides how many times to simulate the written lines.
     const int xLinesInForLoop = 10000;    // Decides how many lines to write per simulation.
 
-    //withoutFlush(timesToRun, xLinesInForLoop);    // Runs the simulation WITHOUT FLUSHING after every line.
-    withFlush(timesToRun, xLinesInForLoop);      // Runs the simulation WITH FLUSHING after every line.
+    // If state = 1, run the sim without flush. If state = 2, run with flush.
+    runSimulation(timesToRun, xLinesInForLoop, 1);
 
     return 0;
 }
